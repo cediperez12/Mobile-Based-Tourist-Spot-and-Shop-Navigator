5aca613015607b37987e73c9d763efc03ea0fcc1
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2019-12-20 14:22:48 +0800

    Changes

--- app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Dashboard.java
+++ app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Dashboard.java
@@ -2,13 +2,53 @@ package com.coecs.mobilebasedtouristspotandshopnavigator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Dashboard extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+
+    private FirebaseUser currentUser;
+
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        init();
+    }
+
+    private void init(){
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        currentUser = auth.getCurrentUser();
+
+        databaseReference = database.getReference();
+
+        //Checks if there is a user logged in to the App
+        if(currentUser == null){
+            toLogin();
+        }
+
+
+    }
+
+    //Navigate to Login
+    private void toLogin(){
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Login.java
+++ app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Login.java
@@ -2,13 +2,133 @@ package com.coecs.mobilebasedtouristspotandshopnavigator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+
+    private DatabaseReference reference;
+
+    private TextInputLayout textInputLayout_email;
+    private TextInputLayout textInputLayout_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init(){
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        reference = database.getReference();
+
+        textInputLayout_email = findViewById(R.id.textinputlayout_email_text);
+        textInputLayout_password = findViewById(R.id.textinputlayout_password);
+    }
+
+    //Login Function
+    public void onClickLogin(View view){
+        try{
+            String textEmail = textInputLayout_email.getEditText().getText().toString().trim();
+            String textPassword = textInputLayout_password.getEditText().getText().toString().trim();
+
+            if(textEmail.isEmpty()){
+                textInputLayout_email.setError("Please enter your email first.");
+                textInputLayout_email.getEditText().addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        if(!s.toString().isEmpty()){
+                            textInputLayout_email.setErrorEnabled(false);
+                        }else{
+                            textInputLayout_email.setErrorEnabled(true);
+                        }
+                    }
+                });
+
+                throw new Exception("Email Not Found");
+            }
+
+            if(textPassword.isEmpty()){
+                textInputLayout_password.setError("Password cannot be empty!");
+                textInputLayout_password.getEditText().addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        if(!s.toString().isEmpty()){
+                            textInputLayout_password.setErrorEnabled(false);
+                        }else{
+                            textInputLayout_password.setErrorEnabled(true);
+                        }
+                    }
+                });
+
+                throw new Exception("Password not Found");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void onClickRegister(View view){
+
+    }
+
+    private class LoginTask extends AsyncTask<Void,Void,String>{
+        String email;
+        String password;
+
+        public LoginTask(String email,String password){
+            this.email = email;
+            this.password = password;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
