65cd38db10d7a9272ef8481c6d93ad95c0cd523c
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2019-12-21 21:20:13 +0800

    Changes

--- app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Login.java
+++ app/src/main/java/com/coecs/mobilebasedtouristspotandshopnavigator/Login.java
@@ -1,14 +1,21 @@
 package com.coecs.mobilebasedtouristspotandshopnavigator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,6 +29,10 @@ public class Login extends AppCompatActivity {
 
     private TextInputLayout textInputLayout_email;
     private TextInputLayout textInputLayout_password;
+    private Button btnLogin;
+    private Button btnRegister;
+
+    private LoginTask loginTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,12 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        loginTask.cancel(true);
     }
 
     private void init(){
@@ -39,6 +56,9 @@ public class Login extends AppCompatActivity {
 
         textInputLayout_email = findViewById(R.id.textinputlayout_email_text);
         textInputLayout_password = findViewById(R.id.textinputlayout_password);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.sign_up_button);
     }
 
     //Login Function
@@ -48,6 +68,7 @@ public class Login extends AppCompatActivity {
             String textPassword = textInputLayout_password.getEditText().getText().toString().trim();
 
             if(textEmail.isEmpty()){
+                textInputLayout_email.setErrorEnabled(true);
                 textInputLayout_email.setError("Please enter your email first.");
                 textInputLayout_email.getEditText().addTextChangedListener(new TextWatcher() {
                     @Override
@@ -74,6 +95,7 @@ public class Login extends AppCompatActivity {
             }
 
             if(textPassword.isEmpty()){
+                textInputLayout_password.setErrorEnabled(true);
                 textInputLayout_password.setError("Password cannot be empty!");
                 textInputLayout_password.getEditText().addTextChangedListener(new TextWatcher() {
                     @Override
@@ -98,18 +120,38 @@ public class Login extends AppCompatActivity {
 
                 throw new Exception("Password not Found");
             }
+
+            loginTask = new LoginTask(textEmail,textPassword);
+            loginTask.execute();
+
         }catch (Exception ex){
             ex.printStackTrace();
         }
     }
 
     public void onClickRegister(View view){
+        try{
 
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private void createErrorMessageWithOkayButton(String title,String message){
+        AlertDialog errorDialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Okay",null)
+                .setCancelable(false)
+                .setIcon(android.R.drawable.stat_sys_warning)
+                .create();
+
+        errorDialog.show();
     }
 
     private class LoginTask extends AsyncTask<Void,Void,String>{
-        String email;
-        String password;
+        private String email;
+        private String password;
 
         public LoginTask(String email,String password){
             this.email = email;
@@ -117,18 +159,49 @@ public class Login extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(Void... voids) {
-            return null;
+        protected void onPreExecute() {
+            super.onPreExecute();
+            btnLogin.setEnabled(false);
+            /*
+            UI thread changes here. Before the Thread runs.
+             */
         }
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+        protected String doInBackground(Void... voids) {
+            try {
+                Thread.sleep(3000);
+
+                auth.signInWithEmailAndPassword(email,password).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        createErrorMessageWithOkayButton("Error Occured while Logging in.",e.getMessage());
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Intent intent = new Intent();
+                        intent.setClass(getApplicationContext(),Dashboard.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            btnLogin.setEnabled(true);
+            /*
+            UI thread changes here. After the Thread runs.
+             */
         }
     }
 }
